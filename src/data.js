5427b4019027a168fb4fe788804b81431207ab6f
Nisha Rai <nisharai26s@gmail.com>
2021-10-15 13:34:02 +0100

    build shopping cart using react

---
+++ src/data.js
@@ -0,0 +1,26 @@
+const data ={
+    products:[
+        {
+    id:"1",
+    name:"Mackbook",
+    price:1400,
+    image:"/image/macbook.jpg"
+},
+{
+    id:"2",
+    name:"AppleWatch",
+    price:800,
+    image:"/image/Apple-Watch.jpg"
+},
+{
+    id:"3",
+    name:"Apple-Phone",
+    price:1000,
+    image:"/image/apple-phone.jpg"
+},
+        
+    ],
+};
+
+
+export default data;
